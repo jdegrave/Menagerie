7b2ae0fea8dd10c36b6d5a80e4db8ce86680d83b
Jodi DeGrave <jdegrave@earthlink.net>
2017-01-29 14:27:37 -0600

    added additional animals

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-/*
-  - Create a new git repository
-  - Create an Express API for the animals of your choice
-    It should contain:
+var express = require('express');  // import express module
+var bodyParser = require('body-parser');  //import for parsing POST and PUT requests
+var routes = require ('./routes');           // routes are organized in one folder
 
-      - At least 3 types of animals
-      - GET, POST, PUT, DELETE requests for each animal
-      - The animals should be stored in their own individual modules
-      - We should be able to sort the animals returned from the API by their ID and one other parameter of your choosing
+var app = express(); //attaches Express functinality to app variable; creates server
 
-   *** Bonus:
+                                // app.use statements processed in order of appearance
+                                // bodyParser must come before routes in order for routes to use it
 
-      - Increment the ID automatically when inserting a new animal
-      - Update only the properties that change when making an update request
-      - Add error handling
+app.use(bodyParser.json());     // use json for POST and PUT
+app.use('/', routes);           // when server starts, this grabs all routes
 
-*/
+app.listen(3000, function() {   // start server
+    console.log('Server listening on port 3000...');
+});
